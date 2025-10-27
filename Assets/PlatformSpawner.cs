e0d934789fe4ef137a37f19e005c9bea218eeb12
codeing-is-hard <rksk99801@gmail.com>
2025-10-27 18:22:46 +0900

    <test>bouncing ball game test code
    1일차 플랫폼 스포너.cs 진행중 큐브와 파란색코인생성 배경은 검은색 10x16 발판역할을해줄 큐브생성

---
+++ Assets/PlatformSpawner.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+
+    [SerializeField] private GameObject platformPrefab;         //발판을 만들기위한 프리팹
+    [SerializeField] private int spawnPlatformCountStart = 12;      //게임 시작시 최초 생성되는 발판의갯수
+    [SerializeField] private float xRange = 10;     //발판이 배치될수있는x좌표의범위(+10~-10range값)
+    [SerializeField]
+
+
+
+
+
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
